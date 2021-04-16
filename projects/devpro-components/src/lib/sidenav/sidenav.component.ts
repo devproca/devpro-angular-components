fe1ede1559bd5d7285e3c66cabdccaa45c038fe0
Colin <colin@dev-pro.ca>
2021-04-16 15:44:31 -0600

    Mirrored the nav header so it works on right hand sides as well.

--- projects/devpro-components/src/lib/sidenav/sidenav.component.ts
+++ projects/devpro-components/src/lib/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
 
 import { Observable } from 'rxjs';
 
@@ -18,8 +18,8 @@ export class SidenavComponent implements OnInit {
   @Input() sideNavTitle: string;
   @Input() sideNavRef: any;
   @Input() sideNavWidth: number = window.innerWidth;
-  @Input() openDirection: SideNavDirection = SideNavDirection.Left;
   @Input() duration = 0.25;
+  @HostBinding('attr.dir') @Input() openDirection: SideNavDirection = SideNavDirection.Left;
 
   constructor(private navService: SidenavService) { }
 
