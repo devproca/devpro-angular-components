7b60e32f7dc433fa231d2ed595d3736e6760495d
Colin <colin@dev-pro.ca>
2021-04-19 14:49:39 -0600

    Redid demo, added basic button.

--- projects/devpro-components/src/lib/button/button.component.ts
+++ projects/devpro-components/src/lib/button/button.component.ts
@@ -7,7 +7,7 @@ import { Component, HostBinding, Input } from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {
-  @HostBinding('attr.color') @Input() color: 'blue' | 'red' = 'blue';
+  @HostBinding('attr.color') @Input() color: 'basic' | 'primary' | 'secondary' = 'basic';
   @HostBinding('attr.stroked') @Input() stroked = false;
   @HostBinding('attr.raised') @Input() raised = false;
   @HostBinding('attr.disabled') @Input() disabled = false;
