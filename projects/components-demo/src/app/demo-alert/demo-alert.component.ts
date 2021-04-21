679adda0f65b41f606e02800fd3fc3e71dc4d56e
Colin <colin@dev-pro.ca>
2021-04-21 11:36:33 -0600

    Alert Demo

--- projects/components-demo/src/app/demo-alert/demo-alert.component.ts
+++ projects/components-demo/src/app/demo-alert/demo-alert.component.ts
@@ -22,4 +22,31 @@ export class DemoAlertComponent implements OnInit {
   clear(): void {
     this.alertService.clearAll();
   }
+
+  codeServiceCall = `import { AlertService } from 'devpro-components';
+  ...
+  constructor(private alertService: AlertService) { ... }
+    ...
+  this.alertService.success('Message', 'Details');`;
+
+  codeHTML = `<dp-alerts></dp-alerts>
+  <dp-button color="primary" (click)="clear()">Clear All</dp-button>`;
+
+  codeTS = `export class DemoAlertComponent implements OnInit {
+
+    constructor(private alertService: AlertService) { }
+
+    ngOnInit(): void {
+      this.alertService.success('This is a Success', 'This is some subtext for detail.');
+      this.alertService.info('This is some Info', 'This is additional useful information.');
+      this.alertService.warning('This is a Warning', 'This is a warning to do something.');
+      this.alertService.danger('This is Dangerous', 'Something has gone wrong!');
+    }
+
+    clear(): void {
+      this.alertService.clearAll();
+    }
+  }`;
+
+
 }
